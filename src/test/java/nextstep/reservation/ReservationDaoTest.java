67a0d0bf1daf74360fd4a92a3ce3c9f3107936f8
kokomo0 <50624525+kokomo0@users.noreply.github.com>
2023-02-02 19:44:24 +0900

    test: DirtiesContext를 사용하도록 변경

--- src/test/java/nextstep/reservation/ReservationDaoTest.java
+++ src/test/java/nextstep/reservation/ReservationDaoTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
@@ -16,6 +17,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 //@JdbcTest
 @SpringBootTest
 @Transactional
--- src/test/java/nextstep/theme/ThemeDaoTest.java
+++ src/test/java/nextstep/theme/ThemeDaoTest.java
@@ -4,12 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 //@JdbcTest
 @Transactional
 @SpringBootTest
